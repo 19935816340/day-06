82d26de578f6193498ddaaedddfe6e54770675f7
19935816340 <63945531+19935816340@users.noreply.github.com>
2020-05-24 00:17:52 +0800

    。。。
    常用API

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s1 = "abc";
+        System.out.println(s1);
+
+	// write your code here
+    }
+}
